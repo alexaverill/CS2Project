05fe039b281faaa28b0e3ca94798429962b939ec
Alex Averill <alex.averill2013@gmail.com>
2014-10-24 23:56:11 -0500

    Adding basic file structure

---
+++ src/CS2Project.java
@@ -0,0 +1,22 @@
+//Default Class for the CS project
+public class CS2Project {
+	private static String preOrder(){
+		//take in a BST and return the preorder transversal
+		String out = "Placeholder"; // this will be the input BST
+		return out;
+	}
+	private static String inOrder(){
+		//take in a BST and return the in order transversal
+		String out = "Placeholder"; // this will be the input BST
+		return out;
+	}
+	private static String postOrder(){
+		//take in a BST and return the post order transversal
+		String out = "Placeholder"; // this will be the input BST
+		return out;
+	}
+	public static void main(String[] args){
+		//Open File input stream.
+		//read in string, and then begin to start building BST
+	}
+}
