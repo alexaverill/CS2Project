bd6fafc43dcedac9de4f32f01343eae3cfd04896
Alex Averill <alex.averill2013@gmail.com>
2014-10-24 23:59:08 -0500

    Fixing comments

--- src/CS2Project.java
+++ src/CS2Project.java
@@ -2,17 +2,17 @@
 public class CS2Project {
 	private static String preOrder(){
 		//take in a BST and return the preorder transversal
-		String out = "Placeholder"; // this will be the input BST
+		String out = "Placeholder"; // this will be the ordered output from BST
 		return out;
 	}
 	private static String inOrder(){
 		//take in a BST and return the in order transversal
-		String out = "Placeholder"; // this will be the input BST
+		String out = "Placeholder"; // this will be the ordered output from BST
 		return out;
 	}
 	private static String postOrder(){
 		//take in a BST and return the post order transversal
-		String out = "Placeholder"; // this will be the input BST
+		String out = "Placeholder"; // this will be the ordered output from BST
 		return out;
 	}
 	public static void main(String[] args){
