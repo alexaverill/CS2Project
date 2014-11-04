3f47ae4963ab152c226643ce293c5b7ef61db1f1
Alex <aaverill@localhost.localdomain>
2014-11-04 14:07:28 -0600

    Adding in more data storage to make display easier

--- src/BinarySearchTree.java
+++ src/BinarySearchTree.java
@@ -6,11 +6,14 @@ public class BinarySearchTree {
 		Node LeftBranch;
 		Node RightBranch;
 		String dataString;
+		String parent;
+		String BranchType; //is this a left or right branch of parent
 		
 		Node (String stringInput){
 			LeftBranch = null;
 			RightBranch = null;
 			dataString = stringInput;
+			
 		}
 	}
 	public void BinarySearchTree(){
